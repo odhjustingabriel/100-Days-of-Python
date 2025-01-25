3817192dab6b52596e22908e9fe482899c7901c2
odhjustingabriel <gabrieljustinodh@gmail.com>
2025-01-25 22:55:29 +0300

    basic math operations

---
+++ Day 2/Age.py
@@ -0,0 +1,12 @@
+#Prompts the user to input their age.
+#Calculates the number of days they’ve lived (approximate it to 365 days per year).
+
+#Prompt user age
+age = int(input("Enter your age: "))
+
+#Calculate the number of days lived
+days_lived = age * 365
+
+#Print the result
+print(f"Your are {age} years old and have lived for {days_lived} days.")
+
