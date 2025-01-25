b9eeaf7e5059e0d692f886a28638914d49d0cb54
odhjustingabriel <gabrieljustinodh@gmail.com>
2025-01-25 20:11:08 +0300

    Day 1: Hello World, Variables, and Data Types

--- Day 1/RectangleArea.py
+++ Day 1/RectangleArea.py
@@ -1,9 +1,15 @@
-# Input: Length and width of the rectangle
+#Input Length and Width of the rectangle
 length = float(input("Enter the length of the rectangle: "))
 width = float(input("Enter the width of the rectangle: "))
 
-# Calculate the area
+#Calculate the perimeter
+perimeter = 2 * (length + width)
+
+#Calculate Area
 area = length * width
 
-# Display the result
+#Print the results
+print("Length: ", length)
+print("Width: ", width)
 print(f"The area of the rectangle with length {length} and width {width} is {area}.")
+print(f"The perimeter of the rectangle with length {length} and width {width} is {perimeter}.")
