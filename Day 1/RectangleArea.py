c1f5ceedbc2b5281240c8c6854c512e124245a1b
odhjustingabriel <gabrieljustinodh@gmail.com>
2025-01-25 19:57:17 +0300

    Day 1: Hello World, Variables, and Data Types

---
+++ Day 1/RectangleArea.py
@@ -0,0 +1,9 @@
+# Input: Length and width of the rectangle
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+
+# Calculate the area
+area = length * width
+
+# Display the result
+print(f"The area of the rectangle with length {length} and width {width} is {area}.")
