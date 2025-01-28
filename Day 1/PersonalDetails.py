98a1d70d0fac6e582da035264ac8336ccedc7ddc
odhjustingabriel <gabrieljustinodh@gmail.com>
2025-01-29 00:45:11 +0300

    Day 1 : Hello World, Variables and Data Types

--- Day 1/PersonalDetails.py
+++ Day 1/PersonalDetails.py
@@ -2,9 +2,9 @@
 #Name, Age, Gender, Favorite Programming Language
 
 # Define variables
-name = "Justin"
-age = 21  # Replace with your age
-favorite_language = "Python"  # Replace with your favorite programming language
+name = input("Enter name: ")
+age = input("Enter age: ")
+favorite_language = input("Enter favorite programming language: ")
 
 # Create the introduction
 introduction = f"Hello! My name is {name}. I am {age} years old, and my favorite programming language is {favorite_language}."
