754db5a7d8c8d8d65ed5e7964aa7a0b245fcd073
odhjustingabriel <gabrieljustinodh@gmail.com>
2025-01-29 00:59:58 +0300

    added main()

--- Day 3/PalindromeChecker.py
+++ Day 3/PalindromeChecker.py
@@ -2,8 +2,13 @@ def is_palindrome(input_string):
     cleaned_string = input_string.replace(" ", "").lower() # Remove spaces and convert to lowercase for uniformity
     return cleaned_string == cleaned_string[::-1] # Check if the string reads the same backward as forward
 
-string = input("Enter a string to check if it's a palindrome: ")
-if is_palindrome(string):
-    print(f'"{string}" is a palindrome.')
-else:
-    print(f'"{string}" is not a palindrome.')
+def main():
+    user_input = input("Enter string: ")
+    
+    if is_palindrome(user_input):
+        print("The string is a palindrome.")
+    else:
+        print("The string is not a palindrome.")
+        
+if __name__ == "__main__":
+    main()
