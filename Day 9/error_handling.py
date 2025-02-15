073b88b586af686a47808259792cbda594c6b015
odhjustingabriel <gabrieljustinodh@gmail.com>
2025-02-15 23:22:49 +0300

    Added loop for retries

--- Day 9/error_handling.py
+++ Day 9/error_handling.py
@@ -1,11 +1,14 @@
 # Error Handling (try-except)
 def divide_numbers():
+   while True:
     try:
         num1 = float(input("Enter the first number: "))
         num2 = float(input("Enter the second number: "))
         
         result = num1 / num2
+        
         print(f"The result of {num1} divided by {num2} is {result}")
+        break
         
     except ZeroDivisionError:
         print("Error! Division by zero is not allowed.")
