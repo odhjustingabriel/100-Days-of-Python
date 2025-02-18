19f23d796cce6341160aab2bfe210c2a8d289cdf
odhjustingabriel <gabrieljustinodh@gmail.com>
2025-02-18 23:49:34 +0300

    Day 10: Regular Expression(Regex)

---
+++ Day 10/validate_email.py
@@ -0,0 +1,26 @@
+import re
+
+def validate_email(email):
+    # Regex pattern for validating an email address
+    pattern = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+    
+    if re.match(pattern, email):
+        print(f"'{email}' is a valid email address.")
+    else:
+        print(f"'{email}' is NOT a valid email address.")
+        
+emails = [
+    "test@example.com",
+    "user.name+tag+sorting@example.com",
+    "user@sub.example.com",
+    "user@123.123.123.123",
+    "user@[IPv6:2001:db8::1]",
+    "invalid-email@",
+    "invalid-email@.com",
+    "no-at-sign.com",
+    "user@.com",
+    "@no-username.com"
+]
+
+for email in emails:
+    validate_email(email)
