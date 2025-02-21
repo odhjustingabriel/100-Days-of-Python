1dc06dda99c47243aa7f264cc62e442dbd9c5d4a
odhjustingabriel <gabrieljustinodh@gmail.com>
2025-02-21 15:11:09 +0300

    Day 09: Error Handling

--- Day 09/error_handling.py
+++ Day 09/error_handling.py
@@ -1,6 +1,7 @@
 # Error Handling (try-except)
 def divide_numbers():
    while True:
+    # try (with block of code to be try)
     try:
         num1 = float(input("Enter the first number: "))
         num2 = float(input("Enter the second number: "))
@@ -10,9 +11,10 @@ def divide_numbers():
         print(f"The result of {num1} divided by {num2} is: {result}")
         break
         
+    # except (with block of code to handle errors)
     except ZeroDivisionError:
         print("Error! Division by zero is not allowed.")
-    except ValueError:
+    except ValueError:  
         print("Error! Invalid input. Please enter valid numbers.")
     except Exception as e:
         print("An unexpected error occurred: {e}")
